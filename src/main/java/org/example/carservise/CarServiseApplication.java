5278e4c096271fa2bcddd9d0e60fcf5801a63dab
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-09-27 21:57:13 +0300

    Initial commit

---
+++ src/main/java/org/example/carservise/CarServiseApplication.java
@@ -0,0 +1,13 @@
+package org.example.carservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/carservise/CarServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
