f31bb94e5b110f9475b89ab045c7ee5e25a04992
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-09-29 16:47:39 +0300

    add @RequestMapping to CarController.java for the primary endpoint

--- src/main/java/org/example/carservise/controllers/CarController.java
+++ src/main/java/org/example/carservise/controllers/CarController.java
@@ -12,11 +12,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/cars")
 public class CarController {
 
     private final CarRepository carRepository;
 
-    @GetMapping("/cars")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Car> getCars(@RequestParam(required = false) Integer minEnginePower,
                              @RequestParam(required = false) Integer maxEnginePower) {
@@ -31,19 +32,19 @@ public class CarController {
         }
     }
 
-    @GetMapping("/cars/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Car> getCar(@PathVariable Long id) {
         return ResponseEntity.of(carRepository.findById(id));
     }
 
-    @PostMapping("/cars")
+    @PostMapping
     public ResponseEntity<Car> createCar(@RequestBody Car car) {
         return ResponseEntity.ok(carRepository.save(car));
     }
 
     @Transactional
-    @PutMapping("/cars/{id}")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<Car> updateCar(@RequestBody Car newCar, @PathVariable(name = "id") Long carId) {
         return ResponseEntity.of(
@@ -58,9 +59,10 @@ public class CarController {
         );
     }
 
-    @DeleteMapping("/cars/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCar(@PathVariable(name = "id") Long carId) {
         carRepository.deleteById(carId);
         return ResponseEntity.ok().build();
     }
 }
+
