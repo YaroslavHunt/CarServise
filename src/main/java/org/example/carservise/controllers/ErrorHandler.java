1616b1633cc01f3b0958254136d5ed4ae78cc666
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-10-10 12:38:28 +0300

    add some new ErrorExceptionDTO, changes in ErrorHandler

--- src/main/java/org/example/carservise/controllers/ErrorHandler.java
+++ src/main/java/org/example/carservise/controllers/ErrorHandler.java
@@ -1,13 +1,14 @@
 package org.example.carservise.controllers;
 
 import org.example.carservise.dto.ErrorDTO;
+import org.example.carservise.dto.ErrorExceptionDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -21,24 +22,29 @@ public class ErrorHandler {
                         .build());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String details = e
-                .getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(error -> error.getField()
-                        .concat(" ")
-                        .concat(error.getDefaultMessage() != null ? error.getDefaultMessage() : "Validation error")
-                )
-            .collect(Collectors.joining("\n"));
+//    @ExceptionHandler({MethodArgumentNotValidException.class})
+//    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+//        String details = e
+//                .getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(error -> error.getField()
+//                        .concat(" ")
+//                        .concat(error.getDefaultMessage() != null ? error.getDefaultMessage() : "Validation error")
+//                )
+//            .collect(Collectors.joining("\n"));
+//
+//        return ResponseEntity
+//                .badRequest()
+//                .body(ErrorDTO.builder()
+//                        .message(details)
+//                        .timestamp(LocalDateTime.now())
+//                        .build());
+//    }
 
-        return ResponseEntity
-                .badRequest()
-                .body(ErrorDTO.builder()
-                        .message(details)
-                        .timestamp(LocalDateTime.now())
-                        .build());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorExceptionDTO handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ErrorExceptionDTO(400, Objects.requireNonNull(e.getFieldError()).getField(), e.getFieldError().getDefaultMessage());
     }
 
 }
