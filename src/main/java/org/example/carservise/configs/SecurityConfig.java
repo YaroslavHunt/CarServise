955ca3bb1a62126dbb72e4b65216eb7c9e415ecd
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-10-18 19:33:05 +0300

    Add security for backend part(OAuth2, Auth0.com)

---
+++ src/main/java/org/example/carservise/configs/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.carservise.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer::disable)
+                .cors(CorsConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/error").permitAll()
+//                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
